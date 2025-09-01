61fd276ca355435a6242b59d9e1191aecf37adf6
marzaananda <marzaanan21@gmail.com>
2025-09-01 22:25:42 +0700

    new update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 export default function About() {
   return (
     <div className="max-w-3xl mx-auto px-6 py-8 pt-[120px]">
-      <h1 className="text-3xl font-bold mb-4">Tentang Kami</h1>
-      <p>Ini adalah halaman tentang kami...</p>
+      <h1 className="text-3xl font-bold mb-4">Sorry</h1>
+      <p>halaman ini masih dalam perbaikan....</p>
     </div>
   );
 }
