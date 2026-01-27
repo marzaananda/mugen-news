c45d9e2227fde4604eb4a7ec8a5aa331e65e7762
marzaananda <marzaanan21@gmail.com>
2026-01-27 16:49:57 +0700

    root:new project

--- src/components/About.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="max-w-3xl mx-auto px-6 py-8 pt-[120px]">
-      <h1 className="text-3xl font-bold mb-4">Sorry</h1>
-      <p>halaman ini masih dalam perbaikan....</p>
-    </div>
-  );
-}
