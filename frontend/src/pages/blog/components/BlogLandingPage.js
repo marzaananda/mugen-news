7e317a4266ad1e4993a5b898ae5748cc35272637
marzaananda <marzaanan21@gmail.com>
2026-02-16 21:02:12 +0700

    new root

---
+++ frontend/src/pages/blog/components/BlogLandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogLandingPage = () => {
+  return (
+    <div>BlogLandingPage</div>
+  )
+}
+
+export default BlogLandingPage
---
+++ frontend/src/pages/blog/components/PostByTags.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostByTags = () => {
+  return (
+    <div>PostByTags</div>
+  )
+}
+
+export default PostByTags
